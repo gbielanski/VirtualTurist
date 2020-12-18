bc5e4d6e1038bc5c53d706fc96607d777d56dcc4
Grzegorz Bielanski <grzegorz.bielanski@glovoapp.com>
2020-12-18 20:54:29 +0100

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Grzegorz Bielanski on 18/12/2020.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
