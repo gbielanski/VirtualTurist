631f46d4fcbbf439c2b7a5e7ba4adba496784e56
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-12-18 21:53:19 +0100

    Add map delegate and read data when map changes

--- VirtualTourist/TravelLocationMapViewController.swift
+++ VirtualTourist/TravelLocationMapViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelLocationMapViewController: UIViewController {
+class TravelLocationMapViewController: UIViewController, MKMapViewDelegate {
 
+  @IBOutlet weak var mapView: MKMapView!
   override func viewDidLoad() {
     super.viewDidLoad()
+    mapView.delegate = self
     // Do any additional setup after loading the view.
+  }
+
+  func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool){
+    print(
+      """
+      regionDidChange
+      \(mapView.region.center.latitude)
+      \(mapView.region.center.longitude)
+      \(mapView.region.span.latitudeDelta)
+      \(mapView.region.span.latitudeDelta)
+      """)
   }
 
 
