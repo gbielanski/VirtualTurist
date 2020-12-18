67960a87a547b64adc52c60390d5971f83b83640
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-12-18 21:30:58 +0100

    Add lauching screen and travel location map view controller

--- VirtualTourist/TravelLocationMapViewController.swift
+++ VirtualTourist/TravelLocationMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelLocationMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Grzegorz Bielanski on 18/12/2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelLocationMapViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
